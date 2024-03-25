af7bbb4240b82995b706f5f29e99789fcb0bcdeb
Nurio Fernández <n.fernandez@efg.gg>
2024-03-25 12:47:41 +0100

    Moved checker from a loop to the controller

--- httpKeys/server.go
+++ httpKeys/server.go
@@ -2,16 +2,21 @@ package httpKeys
 
 import (
 	"fmt"
+	"github.com/nuriofernandez/keys-in-door-experiment/keyscheck"
 	"net/http"
 )
 
-var KeysInDoor bool
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "{\"keysInDoor\":%t}\n", KeysInDoor)
+func status(w http.ResponseWriter, req *http.Request) {
+	keysInDoor, err := keyscheck.AreKeysThere()
+	if err != nil {
+		fmt.Fprintf(w, "{\"error\":1}\n")
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(w, "{\"keysInDoor\":%t}\n", keysInDoor)
 }
 
 func Start() {
-	http.HandleFunc("/status", hello)
+	http.HandleFunc("/status", status)
 	http.ListenAndServe(":8090", nil)
 }
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	_ "github.com/joho/godotenv/autoload" // Keep as first!
-	"github.com/nuriofernandez/keys-in-door-experiment/keyscheck"
-
 	"fmt"
+	_ "github.com/joho/godotenv/autoload" // Keep as first!
 	"github.com/nuriofernandez/keys-in-door-experiment/httpKeys"
-	"time"
 )
 
 func main() {
 	fmt.Println("Starting...")
-	go httpKeys.Start()
-	for {
-		fmt.Println("Checking if keys are in the door...")
-		there, _ := keyscheck.AreKeysThere()
-		httpKeys.KeysInDoor = there
-		time.Sleep(1 * time.Second)
-	}
+	httpKeys.Start()
 }
